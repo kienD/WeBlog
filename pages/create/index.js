9e2bc120d10e47320264c1251a8c6e05b81da280
Kevin Tan <kevin.tan@liferay.com>
2016-12-06 14:42:12 -0800

    Add create post button

--- pages/create/index.js
+++ pages/create/index.js
@@ -5,15 +5,46 @@ import Head from '../Head';
 import Nav from '../Nav';
 
 export default class extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this._handleSubmit = this._handleSubmit.bind(this);
+	}
+	_handleSubmit(event) {
+		event.preventDefault();
+
+		var data = WeDeploy.data('http://data.weblog-test.wedeploy.io');
+
+		data.create(
+			'post',
+			{
+				"title": this.refs.title.value,
+				"contentMarkdown": this.refs.editor.state._value,
+				"contentHTML": this.refs.editor.state._preview
+			}
+		).then(function(post) {
+			console.log(post);
+		});
+	}
+
 	render() {
 		return (
 			<div>
+				<script src="http://cdn.wedeploy.com/api/latest/wedeploy.js"></script>
+
 				<Head />
+
 				<Nav />
 
 				<h1>Create new post!</h1>
 
-				<Editor />
+				<form onSubmit={this._handleSubmit}>
+					<input label="title" type="text" ref="title"/>
+
+					<Editor ref="editor" />
+
+					<button className="button-submit" type="submit">Create Post</button>
+				</form>
 			</div>
 		);
 	}
