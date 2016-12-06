b5f11b6c8b05d8ebf2c94e6eb8de04afe2cfd3ab
Evan Thibodeau <evan.thibodeau@liferay.com>
2016-12-06 14:32:16 -0800

    Use Link Tag

--- pages/Nav.js
+++ pages/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import css from 'next/css';
+import Link from 'next/link'
 import * as utils from '../static/utils.js';
 
 export default class extends React.Component {
 	render() {
 		return (
 			<nav {...css(utils.blockContainer, utils.noPadding)}>
-				<a className={utils.standardPadding} href="/">Home</a>
-				<a className={utils.standardPadding} href="/create">Create New Blog</a>
+				<span className={utils.standardPadding}><Link href="/">Home</Link></span>
+				<span className={utils.standardPadding}><Link href="/create">Create New Blog</Link></span>
 			</nav>
 		);
 	}
