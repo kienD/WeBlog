0417af3a86f0877c7bd9917ed0f0b4898e8ff674
Phillip_Chan <phillip.chan@liferay.COM>
2016-12-06 15:14:40 -0800

    Add header component

---
+++ pages/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import css from 'next/css';
+import Nav from './Nav';
+import * as utils from '../static/utils.js';
+
+
+export default class extends React.Component {
+	render() {
+		return (
+			<header className={utils.largePadding}>
+
+			{this.props.title ? (
+			  <h1 {...css()} className={title}>{this.props.title}</h1>
+			) : null}
+
+			<Nav />
+			</header>
+		);
+	}
+}
+
+const title = css({
+  fontSize: '2.5rem'
+})
